77f81af21da7c894e6d274b9007413ecd5fb5fdf
Paul Ambrose <pambrose@mac.com>
2017-01-25 10:39:46 -0800

    Branch test

--- test_grpc_server.py
+++ test_grpc_server.py
@@ -2,9 +2,8 @@ import logging
 import time
 from threading import Thread
 
-from position_server import PositionServer
-
 from common_constants import LOGGING_ARGS
+from common_utils import sleep
 from location_server import LocationServer
 
 
@@ -17,27 +16,7 @@ def test_location_server(port):
         time.sleep(1)
 
 
-def test_position_server(port):
-    server = PositionServer(port)
-    try:
-        server.start()
-    except BaseException as e:
-        logging.error("Unable to start position server [{0}]".format(e))
-
-    for i in range(0, 100):
-        server.write_position(in_focus=True if i % 2 == 0 else False,
-                              mid_offset=i,
-                              degrees=i + 1,
-                              mid_line_cross=i + 2,
-                              width=i + 3,
-                              middle_inc=i + 4)
-        time.sleep(1)
-
-
 if __name__ == "__main__":
     logging.basicConfig(**LOGGING_ARGS)
-
     Thread(target=test_location_server, args=(50052,)).start()
-    Thread(target=test_position_server, args=(50053,)).start()
-    while True:
-        time.sleep(60)
+    sleep()
